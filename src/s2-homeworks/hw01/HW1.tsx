e634cbe65275adf08c7c972aa02e4750807a2fe0
Maxim <bologa93@mail.ru>
2025-04-23 20:10:41 +0200

    last commit

--- src/s2-homeworks/hw01/HW1.tsx
+++ src/s2-homeworks/hw01/HW1.tsx
@@ -30,7 +30,7 @@ export const message0: MessageType = {
         name: 'Some Name',  // можно менять
     },
     message: {
-        text: 'some textsome textsome textsome textsome textsome textsome text', // можно менять
+        text: 'some макс textsome textsome textsome text', // можно менять
         time: '22:00', // можно менять
     },
 }
